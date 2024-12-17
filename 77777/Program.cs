eb2047cbe53b063361a3c895972021fef80eb641
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-12-17 14:15:47 +0200

    Add files via upload

---
+++ 77777/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _77777
+{
+    class Program
+    {
+        static void Outputnum(int a)
+        {
+            Console.WriteLine(a);
+        }
+        static void Output(List<string> list)
+        {
+            foreach(var el in list)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine();
+        }
+        static void Caps(List<string> list)
+        {
+            foreach (var el in list)
+            {
+                Console.WriteLine(el.ToUpper());
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //Array list = new ArrayList();
+            List<string> list = new List<string>() { "Мария", "Людмила", "Светалана", "Наталья", "Ольга", "Елена" };
+            list.Add("Екатерина");
+            Output(list);
+            Caps(list);
+            Outputnum(list.Count);
+            Console.WriteLine();
+            list.Add("Ника");
+            Output(list);
+            list.Insert(0, "Дарья");
+            Output(list);
+            list.RemoveAt(3);
+            Output(list);
+            list.Reverse();
+            Output(list);
+            Console.ReadLine();
+        }
+    }
+}
